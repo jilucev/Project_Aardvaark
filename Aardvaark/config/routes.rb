3e4b9a27755f444810f95de3db9077eb9ce6894d
jilucev <jilucev7@gmail.com>
2014-04-22 19:32:57 +0000

    basic routes defined

--- Aardvaark/config/routes.rb
+++ Aardvaark/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'home#show'
+
+  namespace :admin do
+    resources :users
+  end
+
+  resources :events 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
