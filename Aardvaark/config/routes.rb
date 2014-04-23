4dc51e1ef121c3c7ab30e4379db098b887d79935
jilucev <jilucev7@gmail.com>
2014-04-23 18:51:11 +0000

    rmv admin namespace

--- Aardvaark/config/routes.rb
+++ Aardvaark/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :events 
   resources :users
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
