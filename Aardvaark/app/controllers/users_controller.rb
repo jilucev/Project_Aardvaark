eac5a93b3beafdd97456c344cc2773258522e5a0
jilucev <jilucev7@gmail.com>
2014-04-24 20:36:43 +0000

    forms with corresponding methods on controllers

--- Aardvaark/app/controllers/users_controller.rb
+++ Aardvaark/app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.user_id
+      redirect_to users_path, notice: "Hello, #{@user.firstname}!"
+    else
+      render :new
+    end
   end
 
   def edit
