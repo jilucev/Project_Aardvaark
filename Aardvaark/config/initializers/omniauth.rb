b88582acd9b0f13b91f497b4f4f1617e8c00d566
jilucev <jilucev7@gmail.com>
2014-04-24 00:04:10 +0000

    on second thought...

--- Aardvaark/config/initializers/omniauth.rb
+++ Aardvaark/config/initializers/omniauth.rb
@@ -1,3 +1,19 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['iQKX1P24Zj5DYb19wvRxcozNi'], ENV['CONSUMER_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
+
+
+# Organization
+# Information about the organization or company associated with your application. This information is optional.
+# Organization  None
+# Organization website  None
+# Application settings
+# Your application's API keys are used to authenticate requests to the Twitter Platform.
+# Access level   Read-only (modify app permissions)
+# API key  iQKX1P24Zj5DYb19wvRxcozNi (manage API keys)
+# Callback URL  None
+# Sign in with Twitter  No
+# App-only authentication https://api.twitter.com/oauth2/token
+# Request token URL https://api.twitter.com/oauth/request_token
+# Authorize URL https://api.twitter.com/oauth/authorize
+# Access token URL  https://api.twitter.com/oauth/access_token
