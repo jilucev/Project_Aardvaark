fb75fa9a80d7573f57e265311e03ed9575bc875a
James Romanchuk <romanchukenator@gmail.com>
2014-04-28 21:19:59 +0000

    Failing at CSS and JS

--- Aardvaark/app/controllers/sessions_controller.rb
+++ Aardvaark/app/controllers/sessions_controller.rb
@@ -8,14 +8,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password]) && !user.organization
       session[:user_id] =  user.id
       redirect_to users_path
-      puts "A mutherfrakkin' VO-LUN-TEER!"
     elsif user && user.authenticate(params[:password])
       session[:user_id] =  user.id
       redirect_to organizations_path
-      puts "A mutherfrakkin' OR-GA-NY-ZAY-HAY-HAY-HAAAAAY-SHUN!"
     else
+      #Fix this
       render :new
-      puts "Fuuuuuck."
     end
     
   end
