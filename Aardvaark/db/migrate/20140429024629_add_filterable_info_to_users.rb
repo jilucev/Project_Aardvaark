5042052428df85c6f985adb24caa5a09e23be281
jilucev <jilucev7@gmail.com>
2014-04-29 02:50:36 +0000

    laying foundation for later migration on users

---
+++ Aardvaark/db/migrate/20140429024629_add_filterable_info_to_users.rb
@@ -0,0 +1,7 @@
+class AddFilterableInfoToUsers < ActiveRecord::Migration
+  # def change
+  #   add_column :users, :gender, :string
+  #   add_column :users, :skills, :text
+  #   add_column :users, :age, :integer
+  # end
+end
