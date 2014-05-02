69ce0e7ffd2ac082e904230e2d23419c7d6fdeda
James Romanchuk <romanchukenator@gmail.com>
2014-05-02 19:18:13 +0000

    Org modifying own events, cool.

--- Aardvaark/app/controllers/events_controller.rb
+++ Aardvaark/app/controllers/events_controller.rb
@@ -31,6 +31,13 @@ class EventsController < ApplicationController
   end
 
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update_attributes(event_params)
+      redirect_to organization_path(params[:organization_id])
+    else
+      render :edit
+    end
   end
 
   def destroy
