b668fcbfd1e55332bc74307b2231128500aa1a05
jilucev <jilucev7@gmail.com>
2014-05-04 23:29:36 +0000

    lalala

--- Aardvaark/app/controllers/sessions_controller.rb
+++ Aardvaark/app/controllers/sessions_controller.rb
@@ -36,44 +36,6 @@ class SessionsController < ApplicationController
       # end
   end
 
-  def check_profile(current_user)
-    if !user_message(current_user)
-      puts "Welcome back, #{current_user.firstname}"
-    else
-      render :new, notice: "#{@message}"
-    end
-  end
-    
-
-  def user_message(current_user)
-    add_to_message = ""
-    @message = "We still need your #{add_to_message}. Please complete your profile."
-    case current_user
-      when !current_user.firstname
-        add_to_message += 'first name' 
-      when !current_user.lastname
-        add_to_message += 'last name'
-      when !current_user.email
-        add_to_message += 'email'
-      when !current_user.phone
-        add_to_message += 'phone number'
-      when !current_user.gender
-        add_to_message += 'gender'
-      when !current_user.skills
-        add_to_message += 'skills'
-      when !current_user.age
-        add_to_message += 'age'
-      when !current_user.vehicle
-        add_to_message += 'vehicle'
-      when !current_user.passenger_capacity
-        add_to_message += 'passenger capacity'
-      else
-        @message = nil
-      end
-      @message
-  end
-
-
   def organization_assign
     organization = Organization.find_by(email: params[:email])
       
