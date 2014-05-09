ddc4a232e8ba2449dcc29776b74babfcd1c84ad9
James Romanchuk <romanchukenator@gmail.com>
2014-05-09 16:56:23 +0000

    Mailer model created

---
+++ Aardvaark/app/models/a_dv_k_mailer.rb
@@ -0,0 +1,2 @@
+class ADvKMailer < ActiveRecord::Base
+end
---
+++ Aardvaark/db/migrate/20140509165604_create_a_dv_k_mailers.rb
@@ -0,0 +1,8 @@
+class CreateADvKMailers < ActiveRecord::Migration
+  def change
+    create_table :a_dv_k_mailers do |t|
+
+      t.timestamps
+    end
+  end
+end
